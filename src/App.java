068fdd57686c73505f9831caf4ad95f4eb4eb33d
dippa-1 <dominik.helfenstein@gmx.de>
2022-02-05 15:19:08 +0100

    new comment

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class App {
   public static void main(String[] args) throws Exception {
@@ -17,6 +15,7 @@ public class App {
       switch (argParser.getCommand()) {
         case ADD:
           LocalDate date;
+          // because the date is optional, we need to check if it was provided
           if (argParser.getDate() == null) {
             ArrayList<SleepEntry> entries = db.getEntries();
             int entriesSize = entries.size();
@@ -83,6 +82,9 @@ public class App {
             SLEEP OPTIMIZER USAGE:
             Add entry: java SleepOptimizer add [-d --date YYYY:MM:dd] [-b --bed HH:MM] [-w --wakeup HH:MM] [-r --rating 0 to 5]
               Example: java SleepOptimizer add -d 2021-01-30 -b 22:55 -w 9:05 -r 3
+            List entries: java SleepOptimizer list
+            Help: java SleepOptimizer help
+            Stats: java SleepOptimizer stats
             """);
   }
 }
