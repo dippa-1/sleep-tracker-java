7612ae286aee04240c3bc64e0e8d8bab51f21df3
dippa-1 <dominik.helfenstein@gmx.de>
2022-02-20 18:45:49 +0100

    x-axis is now labeled but without year numbers

--- src/main/java/TimeSeriesPanel.java
+++ src/main/java/TimeSeriesPanel.java
@@ -1,6 +1,11 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import javax.swing.JPanel;
 
 public class TimeSeriesPanel extends JPanel {
@@ -19,19 +24,37 @@ public class TimeSeriesPanel extends JPanel {
 
     Graphics2D g2 = (Graphics2D) g;
 
-    // constants
-    final int paddingX = 20;
-    final int paddingY = 20;
+    // padding
+    final int pl = 100; // left
+    final int pr = 20; // right
+    final int pt = 20; // top
+    final int pb = 40; // bottom
     final int axisWidth = 2;
     final int height = this.getHeight();
     final int width = this.getWidth();
+    final int usableWidth = width - pl - pr;
+    final int usableHeight = height - pt - pb;
+    final int numTicksDateAxis = 10;
+    final int estimatedWidthPerMonthLabel = 25; // to kind of center date labels
 
     // draw x-axis
     g2.setColor(Constants.primaryColor);
-    g2.fillRect(paddingX, height - paddingY, width - paddingX, height - paddingY - 1);
+    g2.fillRect(pl, height - pb, width - pl - pr, axisWidth);
+    // ticks with date labels
+    LocalDateTime startDate = LocalDateTime.of(this.timeSeries[0].getStartDate(), LocalTime.MIN);
+    LocalDateTime endDate = LocalDateTime.of(this.timeSeries[0].getEndDate(), LocalTime.MIN);
+    Duration timeDiff = Duration.between(startDate, endDate);
+    for (int i = 0; i < numTicksDateAxis; ++i) {
+      LocalDate date = this.timeSeries[0].getStartDate().plusDays(i * timeDiff.toDays() / (numTicksDateAxis - 1));
+      int x = pl + usableWidth * i / (numTicksDateAxis - 1);
+      g2.fillRect(x, height - pb, axisWidth, axisWidth*4);
+
+      g2.drawString(date.getMonth().toString().substring(0, 3), x - estimatedWidthPerMonthLabel / 2, height - pb + 20);
+    }
 
     // draw y-axis
-    // g2.fillRect(paddingX, paddingY, paddingX + axisWidth, getHeight() - paddingY);
+    g2.setColor(Constants.primaryColor);
+    g2.fillRect(pl, pt, axisWidth, height - pt - pb);
 
   }
 }
