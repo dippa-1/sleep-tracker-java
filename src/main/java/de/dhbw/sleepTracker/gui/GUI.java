8b4c543fd07568f102f023448f15edeb202ad80c
dippa-1 <dominik.helfenstein@gmx.de>
2022-02-26 12:32:26 +0100

    cleanup

--- src/main/java/de/dhbw/sleepTracker/gui/GUI.java
+++ src/main/java/de/dhbw/sleepTracker/gui/GUI.java
@@ -1,19 +1,14 @@
 package de.dhbw.sleepTracker.gui;
+
 import java.awt.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import de.dhbw.charts.ChartPanel;
 import de.dhbw.charts.LineChartPanel;
 import de.dhbw.charts.TimeSeries;
-import de.dhbw.charts._ChartPanel;
 import de.dhbw.sleepTracker.core.Database;
-import de.dhbw.sleepTracker.core.SleepEntry;
 
 public class GUI extends JFrame {
     public static void main(String[] args) {
@@ -28,7 +23,6 @@ public class GUI extends JFrame {
         JPanel container = new JPanel();
         container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
         gui.add(container);
-
 
         LineChartPanel chartPanel = new LineChartPanel("Sleep History", new TimeSeries[] {});
         ChartDataManager chartDataManager = new ChartDataManager(db.getEntries(), chartPanel);
